78f9ca9517f85603646e027fe618aac1ba7d7465
adarsh maharjan <adarsh986030@gmail.com>
2024-04-21 11:22:09 +0545

    refactor: refactoed selection sort

--- deletion_in_binary_tree.py
+++ deletion_in_binary_tree.py
@@ -15,41 +15,45 @@ class Node:
         self.left = None
         self.right = None
 
+    def __repr__(self) -> str:
+        return f"{self.data}"
+
 
 def inorder(temp):
     if not temp:
         return
     inorder(temp.left)
-    print(temp.data)
+    print(temp.data, end=" ")
     inorder(temp.right)
+
+
+# delete the given deepest node
 
 
 def deleteDeepest(root, d_node):
     q = []
     q.append(root)
+
     while len(q):
         temp = q.pop(0)
         if temp is d_node:
             temp = None
             return
-
         if temp.right:
             if temp.right is d_node:
                 temp.right = None
                 return
             else:
                 q.append(temp.right)
-        if temp.left:
-            if temp.left is d_node:
-                temp.left = None
-                return
-            else:
-                q.append(temp.left)
+
+
+#  delete element in binary tree
 
 
 def deletion(root, key):
     if root == None:
         return None
+
     if root.left == None and root.right == None:
         if root.key == key:
             return None
@@ -58,9 +62,10 @@ def deletion(root, key):
 
     key_node = None
     q = []
-    temp = None
     q.append(root)
+    temp = None
 
+    # find a node to be deleted
     while len(q):
         temp = q.pop(0)
         if temp.data == key:
@@ -69,25 +74,27 @@ def deletion(root, key):
             q.append(temp.left)
         if temp.right:
             q.append(temp.right)
+
+    # replace the delete node with rigtmost node
     if key_node:
         x = temp.data
         key_node.data = x
         deleteDeepest(root, temp)
-
     return root
 
 
 if __name__ == "__main__":
-    root = Node(10)
-    root.left = Node(11)
-    root.left.left = Node(7)
-    root.left.right = Node(12)
-    root.right = Node(9)
-    root.right.left = Node(15)
-    root.right.right = Node(8)
-    print("The tree before the deletion: ", end="")
+    root = Node(13)
+    root.left = Node(12)
+    root.left.left = Node(4)
+    root.left.right = Node(19)
+    root.right = Node(10)
+    root.right.left = Node(16)
+    root.right.right = Node(9)
+
+    print("The tree before the deletion: ", end=" ")
     inorder(root)
-    key = 11
+    key = 12
     root = deletion(root, key)
     print()
     print("The tree after the deletion: ", end="")
--- sorting/selection-sort.py
+++ sorting/selection-sort.py
@@ -1,11 +1,17 @@
 A = [64, 25, 12, 22, 11]
-print("Unsorted Array", A)
 
-for i in range(len(A)):
-    min_idx = i
-    for j in range(i + 1, len(A)):
-        if A[min_idx] > A[j]:
-            min_idx = j
-    A[i], A[min_idx] = A[min_idx], A[i]
 
-print(f"Sorted Array: {A}")
+def selectionSort(arr):
+    unsorted_arr = arr
+    arr_length = len(unsorted_arr)
+    for i in range(arr_length):
+        min_idx = 1
+        for j in range(i + 1, arr_length):
+            if unsorted_arr[min_idx] > unsorted_arr[j]:
+                min_idx = j
+        unsorted_arr[min_idx], unsorted_arr[i] = unsorted_arr[i], unsorted_arr[min_idx]
+    return unsorted_arr
+
+
+res = selectionSort(A)
+print(f"Sorted Array: {res}")
